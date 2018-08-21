88be5615d06ef6ed5239c5ba1d3d91e9b9b6509d
Marius von Hassel <m.vonhassel@chal-tec.com>
2018-08-21 18:08:15 +0200

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Icon } from 'react-materialize';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
